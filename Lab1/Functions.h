4f154f7e258d8c0c56c4cfc8d513ec38be3f1ab7
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2018-08-30 21:03:17 -0400

    Created rest of prototype functions to be used in credit card checksum.

--- Lab1/Functions.h
+++ Lab1/Functions.h
@@ -4,3 +4,28 @@ void menu();
 void QuadraticRoot();
 void CreditCardValidator();
 void GradeCalculator();
+
+//Functions that make up CreditCardValidator()
+
+//Return true if the card number is valid.
+bool isValid(long long number);
+
+//Get the result of step 2
+int sumOfDoubleEvenPlace(long long number);
+
+//Return this number if it is a single digit;
+//Otherwise, return the sum of the two digits.
+int getDigit(int number);
+
+//Return sum of odd-place digits in number.
+int sumOfOddPlace(long long number);
+
+//Return true if the digit is a prefix for this number.
+bool prefixMatched(long long number, int digit);
+
+//Return the number ofdigits in number
+int getSize(long long number);
+
+//Return the first numDigits digits from number. If the # of
+//digits in number is less than numDigits, return number.
+long getPrefix(long long number, int numDigits);
