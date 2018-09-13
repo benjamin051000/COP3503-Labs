c792acb2b719e4c393c6a977a6ebd4288b1193a0
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2018-09-13 07:51:33 -0400

    Lab 3

---
+++ Lab3/Lab3/Lab3/DynamicArray.h
@@ -0,0 +1,148 @@
+#pragma once
+
+template<typename T>
+class DynamicArray {
+public:
+	//Accessors
+	unsigned int GetCapacity() const;
+	unsigned int GetSize() const;
+	const T* GetData() const;
+
+	//Return a specific element from the internal array
+	const T& operator[](unsigned int index) const;
+	T& operator[](unsigned int index);
+	
+	//Just like the brakcets operator, but with a function
+	const T& At(unsigned int index) const;
+	T &At(unsigned int index);
+
+	//Mutators
+	//Add an item onto the end of the array, if there is room.
+	//If not, resize it to accomodate space
+	void Add(const T& element);
+
+	//This is the beefcake of a function that does the resizing.
+	void Resize(unsigned int newSize);
+
+	//Removes an element from an array, 
+	//and then shrinks the array to fill the gap.
+	void Remove(unsigned int index);
+
+	//Constructors
+	DynamicArray();
+	DynamicArray(unsigned int);
+
+	//Trilogy of Evil
+	DynamicArray(const DynamicArray& d);
+	DynamicArray& operator=(const DynamicArray& d);
+	~DynamicArray();
+
+private:
+	//Pointer to the array which holds the data
+	T* data;
+	//Number of elements in data
+	unsigned int size;
+	//Maximum size of the current array
+	unsigned int capacity;
+
+	void set(const DynamicArray& d);
+};
+
+/*====================METHOD=IMPLEMENTATIONS==========================*/
+
+template<typename T>
+DynamicArray<T>::DynamicArray(unsigned int size) {
+	size = 0;
+	size = size; //this-> ?
+	data = new T[size];
+}
+
+template<typename T>
+void DynamicArray<T>::Remove(unsigned int index) {
+	for (unsigned int i = index; i < size - 1; i++) {
+		data[i] = data[i + 1];
+	}
+
+	Resize(capacity - 1);
+}
+
+template<typename T>
+DynamicArray<T>::DynamicArray() {
+	capacity = 0;
+	capacity = 10;
+
+	data = new T[capacity];
+}
+
+template<typename T>
+DynamicArray<T>::~DynamicArray() {
+	delete[] data;
+}
+
+template<typename T>
+DynamicArray<T>::DynamicArray(const DynamicArray& d) {
+	set(d);
+}
+
+template<typename T>
+DynamicArray<T>& DynamicArray<T>::operator=(const DynamicArray<T>& d) {
+	set(d);
+	return *this;
+}
+
+template<typename T>
+void DynamicArray<T>::set(const DynamicArray& d) {
+	capacity = d.capacity;
+	size = 0;
+	data = new T[capacity];
+
+	for (unsigned int i = 0; i < capacity; i++) {
+		data[i] = d[i];
+	}
+}
+
+
+template<typename T>
+void DynamicArray<T>::Add(const T& element) {
+	//If the array is full, add a slot
+	if (size >= capacity) {
+		Resize(capacity + 1);
+	}
+
+	//Add the element to the end of the array
+	data[capacity - 1] = element;
+}
+
+template<typename T>
+void DynamicArray<T>::Resize(unsigned int newSize) {
+	T* newArr = new T[newSize];
+
+	for (unsigned int i = 0; i < newSize; i++)
+		newArr[i] = data[i]; //shallow copy?
+
+	if (data != nullptr)
+		delete[] data;
+	
+	data = newArr;
+}
+
+template<typename T>
+unsigned int DynamicArray<T>::GetCapacity() const {return capacity;}
+
+template<typename T>
+unsigned int DynamicArray<T>::GetSize() const {return size;}
+
+template<typename T>
+const T* DynamicArray<T>::GetData() const {return data;}
+
+template<typename T>
+const T& DynamicArray<T>::At(unsigned int index) const {return data[index];}
+
+template<typename T>
+T& DynamicArray<T>::At(unsigned int index) {return data[index];}
+
+template<typename T>
+const T& DynamicArray<T>::operator[](unsigned int index) const {return data[index];}
+
+template<typename T>
+T& DynamicArray<T>::operator[](unsigned int index) {return data[index];}
---
+++ Lab3/Lab3/Lab3/main.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "DynamicArray.h"
+using namespace std;
+
+// Sample class, to show a template can work with anything
+class Hero
+{
+	string name_;
+	int hitpoints_;
+	int maxHP_;
+public:
+	Hero() {}
+	Hero(const char *name, int hp, int maxHP)
+	{
+		name_ = name;
+		hitpoints_ = hp;
+		maxHP_ = maxHP;
+	}
+	void SetName(const char *name)
+	{
+		name_ = name;
+	}
+	string GetName()
+	{
+		return name_;
+	}
+	friend ostream &operator<<(ostream &os, const Hero &h)
+	{
+		cout << h.name_ << " Hitpoints: " << h.hitpoints_ << " / " << h.maxHP_;
+		return os;
+	}
+};
+
+void TestOne();
+void TestTwo();
+void TestThree();
+void TestFour();
+
+int main()
+{
+	cout << "**** Testing the Dynamic Array ****" << endl << endl;
+	TestOne();
+	TestTwo();
+	TestThree();
+	TestFour();
+
+	return 0;
+}
+
+void TestOne()
+{
+	cout << "Integer container: Initial capacity of 0" << endl;
+	DynamicArray<int> data(0);
+	cout << "Adding 5 items..." << endl;
+	for (int i = 1; i < 32; i *= 2)
+		data.Add(i);
+	data[4] = 500;
+	for (unsigned int i = 0; i < data.GetSize(); i++)
+		cout << data[i] << endl;
+
+	cout << "Capacity: " << data.GetCapacity() << endl;
+	cout << "Size: " << data.GetSize() << endl;
+}
+void TestTwo()
+{
+	DynamicArray<float> data2(10);
+	cout << "\nFloat container: Initial capacity of 10" << endl;
+	cout << "Adding 5 items..." << endl;
+
+	for (int i = 0; i < 5; i++)
+		data2.Add((float)101 / (i + 2));
+
+	for (unsigned int i = 0; i < data2.GetSize(); i++)
+		cout << data2[i] << endl;
+
+	cout << "Capacity: " << data2.GetCapacity() << endl;
+	cout << "Size: " << data2.GetSize() << endl;
+
+	cout << "\nShrinking container to 2 elements..." << endl;
+	data2.Resize(2);
+
+	for (unsigned int i = 0; i < data2.GetSize(); i++)
+		cout << data2[i] << endl;
+	cout << "Capacity: " << data2.GetCapacity() << endl;
+	cout << "Size: " << data2.GetSize() << endl;
+
+	cout << "Attempting to access index[50]...";
+	try { cout << data2[50] << endl; }
+	catch (const char *msg)
+	{
+		cout << msg << endl;
+	}
+}
+void TestThree()
+{
+	DynamicArray<int> data3;
+
+	data3.Add(100);
+	data3.Add(999);
+	data3.Add(200);
+	data3.Add(300);
+	data3.Add(400);
+
+	for (unsigned int i = 0; i < data3.GetSize(); i++)
+		cout << data3[i] << endl;
+
+	cout << "Removing '999' from the list... " << endl;
+	data3.Remove(1);
+
+	for (unsigned int i = 0; i < data3.GetSize(); i++)
+		cout << data3[i] << endl;
+
+	cout << "Creating a copy of the list (testing the copy constructor)... " << endl;
+	auto data4 = data3;
+
+	cout << "\nPrinting copy of dynarray..." << endl;
+	for (unsigned int i = 0; i < data4.GetSize(); i++)
+		cout << data4[i] << endl;
+
+	cout << "\nAttempting to remove index[100]..." << endl;
+	try { data3.Remove(100); }
+	catch (const char *msg)
+	{
+		cout << msg << endl;
+	}
+
+}
+void TestFour()
+{
+	DynamicArray<Hero> heroes;
+	heroes.Add(Hero("Conan", 100, 120));
+	heroes.Add(Hero("Thor", 150, 150));
+	heroes.Add(Hero("Merlin", 80, 85));
+	heroes.Add(Hero("Bob", 25, 26));
+	cout << "\nPrinting heroes from dynarray..." << endl;
+
+	for (unsigned int i = 0; i < heroes.GetSize(); i++)
+		cout << heroes[i] << endl;
+
+	DynamicArray<Hero> heroClones;
+	cout << "\nCloning our heroes (testing the assignment operator)... ";
+	heroClones = heroes;
+
+	cout << "\nPrinting cloned heroes from dynarray (and changing their names)..." << endl;
+	for (unsigned int i = 0; i < heroClones.GetSize(); i++)
+	{
+		string name = "Evil " + heroClones[i].GetName();
+		heroClones[i].SetName(name.c_str());
+		cout << heroClones[i] << endl;
+	}
+}
