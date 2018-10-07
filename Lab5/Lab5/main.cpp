38fc703b512043f946e59658038c0b9422627bc1
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2018-10-07 13:41:41 -0400

    yeet

--- Lab5/Lab5/main.cpp
+++ Lab5/Lab5/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <iomanip>
-
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
+
+class Lego {
+	int setNumber;
+	string theme;
+	string name;
+	int numMiniFigs;
+	int numPieces;
+	float price;
+
+public:
+	
+};
 
 int main() {
 	cout << std::fixed << setprecision(2);
@@ -12,9 +26,46 @@ int main() {
 	cout << "4. All 3 files" << endl;
 	int option;
 	cin >> option;
+	string filename;
+	ifstream file;
+	bool sample = true;
+	vector<Lego> parsedData;
 
 	/*======= Load data from file(s) =======*/
+	switch (option) {
+	case 1: filename = sample ? "SAMPLE_lego1.csv" : "lego1.csv";
+		break;
+	case 2: filename = sample ? "SAMPLE_lego2.csv" : "lego2.csv";
+		break;
+	case 3: filename = sample ? "SAMPLE_lego3.csv" : "lego3.csv";
+		break;
+	case 4: filename = "lego4.csv";
+		break;
+	default: cout << "Invalid input." << endl;
+		return 0;
+	}
+
+	file.open(filename);
+	if (!file.is_open()) {
+		cout << "Could not open " << filename << "." << endl;
+		return -1;
+	}
+
+	//load em up
+	int setNumber;
+	string theme;
+	string name;
+	int numMiniFigs;
+	int numPieces;
+	float price;
+	string line;
+	while (getLine(file, line)) {
+
+	}
+
+
 	/*======= Print out how many sets were loaded =======*/
+	cout << parsedData.size() << " sets loaded." << endl;
 
 	/* Imagine your program had a menu like this:
 	cout << "1. Most Expensive set" << endl;
@@ -32,8 +83,29 @@ int main() {
 	cin.get();  // Clear newline character for any later input
 
 	/*======= Based on the choice, execute the appropriate task and show the results =======*/
+	switch(choice) {
+	case 1: 
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		break;
+	case 8:
+		break;
+	case 0: return 0;
+		break;
+	default:
+	}
 
+	file.close();
 	return 0;
 }
-
 
