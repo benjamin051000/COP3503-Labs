ed1ed4af75f8c963dcc2e191eb1d0e16b1861cb8
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2018-10-07 21:26:38 -0400

    files work! Now do all the damn methods. ugh

--- Lab5/Lab5/main.cpp
+++ Lab5/Lab5/main.cpp
@@ -7,24 +7,47 @@
 using namespace std;
 
 struct Lego {
-	int setNumber;
-	string theme;
-	string name;
-	int numMiniFigs;
-	int numPieces;
-	float price;
+	int setNumber = 0;
+	string theme = "";
+	string name = "";
+	int numMiniFigs = 0;
+	int numPieces = 0;
+	float price = 0.0f;
 
-	Lego(int setNumber = -1, string theme = "", string name = "", int numMiniFigs = 0, int numPieces = 0, float price = 0);
+	Lego() {}
+
+	Lego(int setNumber, string theme, string name, int numMiniFigs, int numPieces, float price) {
+		this->setNumber = setNumber;
+		this->theme = theme;
+		this->name = name;
+		this->numMiniFigs = numMiniFigs;
+		this->numPieces = numPieces;
+		this->price = price;
+	}
+
+	friend ostream& operator<<(ostream& os, Lego& set) {
+		/*os << "Name: " << this->name << "\nNumber: " << this->setNumber << "\nTheme: " << this->theme
+			<< "\nPrice: $" << this->price << "\nMinifigures: " << this->numMiniFigs
+			<< "\nPiece count: " << this->numPieces;*/
+		os << "Name: " << name << "\nNumber: " << setNumber << "\nTheme: " << theme
+			<< "\nPrice: $" << price << "\nMinifigures: " << numMiniFigs
+			<< "\nPiece count: " << numPieces;
+		return os;
+	}
 };
 
-void testLego() {
+void mostExpensiveSet();
+void largestPieceCount();
+void searchName();
+void searchTheme();
+void partCountInfo();
+void priceInfo();
+void minifigInfo();
+void buyItAll();
 
-}
+vector<Lego> parsedData;
 
 int main() {
-	testLego();
-	return 0;
-
 	cout << std::fixed << setprecision(2);
 	cout << "Which file(s) to open?\n";
 	cout << "1. lego1.csv" << endl;
@@ -33,10 +56,11 @@ int main() {
 	cout << "4. All 3 files" << endl;
 	int option;
 	cin >> option;
+
 	string filename;
 	ifstream file;
 	bool sample = true;
-	vector<Lego> parsedData;
+	
 
 	/*======= Load data from file(s) =======*/
 	switch (option) {
@@ -46,7 +70,7 @@ int main() {
 		break;
 	case 3: filename = sample ? "SAMPLE_lego3.csv" : "lego3.csv";
 		break;
-	case 4: filename = "lego4.csv";
+	case 4: filename = "lego4.csv"; //should open every file
 		break;
 	default: cout << "Invalid input." << endl;
 		return 0;
@@ -58,19 +82,47 @@ int main() {
 		return -1;
 	}
 
-	//load em up
-	int setNumber;
-	string theme;
-	string name;
-	int numMiniFigs;
-	int numPieces;
-	float price;
-	string line;
+	//Load vector with each Lego instnace
+
+	//Attributes
+	int asetNumber;
+	string atheme;
+	string aname;
+	int anumMiniFigs;
+	int anumPieces;
+	float aprice;
+
+	//used to parse the data
+	string line, sstoken;
+	
+	//skip the first line
+	getline(file, line, '\n'); 
 
 	while (getline(file, line, '\n')) {
-		stringstream s(line);
 
+		stringstream ss(line);
 
+		//load each individual variable
+		getline(ss, sstoken, ','); 
+		asetNumber = stoi(sstoken); 
+
+		getline(ss, sstoken, ',');
+		atheme = sstoken;
+		
+		getline(ss, sstoken, ',');
+		aname = sstoken;
+		
+		getline(ss, sstoken, ',');
+		anumMiniFigs = sstoken == "" ? 0 : stoi(sstoken);
+
+		getline(ss, sstoken, ',');
+		anumPieces = sstoken == "" ? 0 : stoi(sstoken);
+
+		getline(ss, sstoken, ',');
+		aprice = sstoken == "" ? 0.0f : stof(sstoken);
+
+		//make a new lego instance and put it in the vector
+		parsedData.push_back(Lego(asetNumber, atheme, aname, anumMiniFigs, anumPieces, aprice));
 	}
 
 
@@ -89,36 +141,67 @@ int main() {
 	 cout << "0. Exit" << endl;
 	*/
 	int choice;
-	cin >> choice;
-	cin.get();  // Clear newline character for any later input
+	do {
+		cin >> choice;
+		cin.get();  // Clear newline character for any later input
 
-	/*======= Based on the choice, execute the appropriate task and show the results =======*/
-	switch(choice) {
-	case 1: 
-		break;
-	case 2:
-		break;
-	case 3:
-		break;
-	case 4:
-		break;
-	case 5:
-		break;
-	case 6:
-		break;
-	case 7:
-		break;
-	case 8:
-		break;
-	case 0: return 0;
-		break;
-	default:
-	}
+		/*======= Based on the choice, execute the appropriate task and show the results =======*/
+		switch (choice) {
+		case 1: mostExpensiveSet();
+			break;
+		case 2:largestPieceCount();
+			break;
+		case 3: searchName();
+			break;
+		case 4: searchTheme();
+			break;
+		case 5: partCountInfo();
+			break;
+		case 6: priceInfo();
+			break;
+		case 7: minifigInfo();
+			break;
+		case 8: buyItAll();
+			break;
+		case 0:
+			break;
+		default:
+			break;
+		}
+	} while (choice != 0); //better way to do this?
 
 	file.close();
 	return 0;
 }
 
-Lego::Lego(int setNumber, string theme, string name, int numMiniFigs, int numPieces, float price) {
+void mostExpensiveSet() {
+	Lego mostExpensiveSet; //will this work?
 
+	for (Lego set: parsedData) {
+		if (set.price > mostExpensiveSet.price) {
+			mostExpensiveSet = set;
+		}
+	}
+	cout << "Most expensive set costs $" << mostExpensiveSet << endl;
+}
+
+void largestPieceCount() {
+}
+
+void searchName() {
+}
+
+void searchTheme() {
+}
+
+void partCountInfo() {
+}
+
+void priceInfo() {
+}
+
+void minifigInfo() {
+}
+
+void buyItAll() {
 }
